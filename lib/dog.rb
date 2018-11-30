3aa7345a24e4a7b9e4fcdf2bc5d599a8cdc72d50
Miguel Henriquez <henriquez.miguel@outlook.com>
2018-11-30 16:07:21 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog()
+  def intialize(name)
+    @name = name
+    end
+  end
+
+d = Dog.new('fido', 'snoopy', 'lassie')
+
+#fido = Dog.new
+#fido.name = "Fido"
+
+#snoopy = Dog.new
+#snoopy.name = "snoopy"
+
+#lassie = Dog.new
+#lassie.name = "lassie"
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+  def initialize(name)
+    @name = name
+    
+end
+
+adele_goldberg = Person.new
+adele_goldberg.name = "adele goldberg"
+
+alan_key = Person.new
+alan_key.name = "alan key"
