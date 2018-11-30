db77789c450171ccc0e3c4d4c16d62e1b8eb1708
Miguel Henriquez <henriquez.miguel@outlook.com>
2018-11-30 15:56:24 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog
+end
+fido = Dog.new
+fido.name = "Fido"
+snoopy = Dog.new
+snoopy.name = "snoopy"
+lassie = Dog.new
+lassie.name = "lassie"
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+end
+
+adele_goldberg = Person.new
+adele_goldberg.name = "adele goldberg"
+alan_key = Person.new
+alan_key.name = "alan key"
